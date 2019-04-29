d4273b3d16920e3890b82e2eb01d050f8cf15d02
kresimir <kresomaric66@gmail.com>
2019-04-29 10:46:42 +0100

    New commit

--- js/script.js
+++ js/script.js
@@ -5,5 +5,15 @@ $(() => {
 	});
 });
 
+// clock function
+$(function() {
+	function Clock() {
+		var theDate = new Date();
+		var t = theDate.getHours() + ':' + theDate.getMinutes() + ':' + theDate.getSeconds();
+		$('#time').text(t);	
+	}
+	setInterval(Clock, 1000);
+});
+
 
 
